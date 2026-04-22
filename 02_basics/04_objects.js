4d2e46abc79746047f9b72241020c830522d0e88
Sandarbh05 <sandarbhvansh039@gmail.com>
2026-04-23 01:46:51 +0530

    Learned advanced object concepts

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,42 @@
+//Object Declaration
+// const tinderUser = new Object() //Singleton object
+const tinderUser = {} //Non Singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullName: {
+        userfullname: {
+            firstname: "Sandarbh",
+            lastname: "Vansh"
+        }
+    }
+}
+
+// console.log(regularUser.fullName?.userfullname.firstname); //Its called optional chaining ? means only enter the nesting if the currently object value exist, mostly used in API responses
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "b"}
+const obj4 = {5: "c", 6: "c"}
+// Merging objects
+// 1)
+// const obj3 = {obj1, obj2} // It will nest the objects instead of just merging
+
+// 2)
+// const obj3 = Object.assign(obj1, obj2)
+// const obj3 = Object.assign({}, obj1, obj2, obj4) //Its optional but gives assurity that only object values will be merge to a target object
+
+// 3)
+const obj3 = {...obj1, obj2}
+console.log(obj3);
+
+console.log(Object.keys(tinderUser)); //Gives list of keys in an Array
+console.log(Object.values(tinderUser)); //Gives list of values in an Array
+console.log(Object.entries(tinderUser)); //Gives each entry of objects key-value pair in sets of Arrays
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); //Check if the object has specific property available or not
